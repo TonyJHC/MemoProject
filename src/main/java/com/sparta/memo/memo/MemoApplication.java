2f7baec10a7d81a6207da4986ea2d455ce3e6d20
TonyJHC <jongvin01@naver.com>
2021-09-06 20:04:11 +0900

    writePost/getMessage/submitEdit
        edit 4489291 message
        edit 4890c62 writePost/getMessage/submitEdit
        edit df98ec9 deleteOne method
        edit 6e4f033 메모 목록 24시간 이내로만 조회

--- src/main/java/com/sparta/memo/memo/MemoApplication.java
+++ src/main/java/com/sparta/memo/memo/MemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@EnableJpaAuditing // 스프링아 우리 Jpa 쓸건데 data 변동이 있으면 꼭 jpa가 자동으로 반영하도록 해줘 ~ -> 이거 없으면 수정/변경 될 때 데이터 반영 안됨
 public class MemoApplication {
 
     public static void main(String[] args) {
