6e4f033f5c84e69ddb6fdfc67920eed2f6ef079f
최종현 <hyun@choejonghyeon-ui-MacBookPro.local>
2021-09-06 21:59:11 +0900

    메모 목록 24시간 이내로만 조회

--- src/main/java/com/sparta/memo/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/memo/controller/MemoController.java
@@ -7,6 +7,8 @@ import com.sparta.memo.memo.service.MemoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 @RestController // --> new MemoController 를 Spring이 자동으로 해줌
@@ -25,7 +27,10 @@ public class MemoController {
 
     @GetMapping("/api/memos")
     public List<Memo> readMemo() {
-        return memoRepository.findAllByOrderByModifiedAtDesc(); // 찾는데 순서대로 수정된 날짜를 기준으로 내림차순
+//        return memoRepository.findAllByOrderByModifiedAtDesc(); // 찾는데 순서대로 수정된 날짜를 기준으로 내림차순
+        LocalDateTime end = LocalDateTime.now(); // 현재 시간
+        LocalDateTime start = LocalDateTime.now().minusDays(1);
+        return memoRepository.findAllByModifiedAtBetweenOrderByModifiedAtDesc(start, end);
     }
 
     @PutMapping("/api/memos/{id}")
